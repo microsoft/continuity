0371a3b2b65da2863173cbe05eb6db29269f618a
Adam Foxman <adam@thefoxmans.net>
2020-03-19 10:25:35 -0700

    Add GoogleTest and GoogleMock as a project dependency. Add a ReadManifest unit test. Add CMake install to produce a re-distributable collection of headers and binaries.

---
+++ tests/Continuity/ManifestTests.cpp
@@ -0,0 +1,56 @@
+#include <Continuity/ReactManifest.h>
+#include <gmock/gmock.h>
+#include <gtest/gtest.h>
+
+using namespace Microsoft::React;
+using namespace testing;
+
+const char* c_jsonManifest = R"(
+{
+    "components": {
+        "MyApp.app": {
+            "displayName": "MyApp",
+            "initialProperties": {
+                "a": "string prop",
+                "b": true,
+                "c": 3.1415925,
+                "d": {
+                    "nested": 88
+                }
+            }
+        }
+    },
+    "runtime": {
+        "devMode": true,
+        "liveReload": true,
+        "attachToWebDebugger": true,
+        "win32": {
+            "container": {
+                "type": "dialog",
+                "width": 450,
+                "height": 400,
+                "showOkButton": true,
+                "showCancelButton": true
+            }
+        }
+    },
+    "assetsPath": "assets",
+    "bundle": {
+        "fileName": "index.win32",
+        "extension": "bundle"
+    },
+    "packager": {
+        "host": "localhost:8081"
+    }
+}
+)";
+
+
+TEST(ManifestTests, ReadManifestSucceeds)
+{
+    IManifest* manifest = nullptr;
+    auto error = ReadManifest(ManifestSource::Host, c_jsonManifest, &manifest);
+
+    ASSERT_EQ(error, ReactError::Success);
+    EXPECT_THAT(manifest, NotNull());
+}
